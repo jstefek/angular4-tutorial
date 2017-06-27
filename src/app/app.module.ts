c911d99bdd31ffaee069dbabd7ac82d83c4e4045
Jiri Stefek <jstefek01@gmail.com>
2017-06-27 11:04:00 +0200

    multiple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
 
 import {AppComponent} from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
 
 @NgModule({
-    imports: [BrowserModule,
+    imports: [
+        BrowserModule,
         FormsModule, // <-- import the FormsModule before binding with [(ngModel)]]
     ],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeroDetailComponent
+    ],
+    bootstrap: [
+        AppComponent,
+    ]
 })
 export class AppModule {}
