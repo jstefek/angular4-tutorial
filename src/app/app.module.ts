ec0b6c82d04e7afa417b0700d34ee383a1f8a095
Jiri Stefek <jstefek01@gmail.com>
2017-06-28 10:54:14 +0200

    http, hero search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {DashboardComponent} from './dashboard.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
+import {HeroSearchComponent} from './hero-search.component';
 import {HeroService} from './hero.service';
 
 @NgModule({
@@ -26,7 +27,8 @@ import {HeroService} from './hero.service';
         AppComponent,
         DashboardComponent,
         HeroDetailComponent,
-        HeroesComponent
+        HeroesComponent,
+        HeroSearchComponent
     ],
     providers: [
         HeroService
